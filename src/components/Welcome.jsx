3fab992ade1d176e762e0e3ae258e450e7127eb1
Hossein Shahverdi <h.shahverdi1997@gmail.com>
2025-11-25 18:23:03 +0330

    implemented header and navbar

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,94 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const FONT_WEIGHTS = {
+  subtitle: { min: 300, max: 900, default: 300 },
+  title: { min: 400, max: 900, default: 400 },
+};
+
+const renderText = (text, className, baseWeight = 400) => {
+  return [...text].map((char, index) => (
+    <span
+      key={index}
+      className={className}
+      style={{ fontVariationSettings: `"wght" ${baseWeight}` }}
+    >
+      {char === " " ? "\u00A0" : char}
+    </span>
+  ));
+};
+
+const setupTextHover = (container, type) => {
+  if (!container) return;
+
+  const letters = container.querySelectorAll("span");
+  const { min, max } = FONT_WEIGHTS[type];
+
+  const animateLetters = (letter, weight, duration = 0.25) => {
+    return gsap.to(letter, {
+      duration,
+      ease: "power2out",
+      fontVariationSettings: `"wght" ${weight}`,
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { left } = container.getBoundingClientRect();
+    const mouseX = e.clientX - left;
+
+    letters.forEach((letter) => {
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX - (l - left + w / 2));
+      const intensity = Math.exp(-(distance ** 2) / 2000);
+
+      animateLetters(letter, min + (max - min) * intensity, 0.25);
+    });
+  };
+
+  const handleMouseLeave = () =>
+    letters.forEach((letter) =>
+      animateLetters(letter, FONT_WEIGHTS[type].default, 0.3)
+    );
+
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener("mouseleave", handleMouseLeave);
+
+  return () => {
+    container.removeEventListener("mousemove", handleMouseMove);
+    container.removeEventListener("mouseleave", handleMouseLeave);
+  };
+};
+
+const Welcome = () => {
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  useGSAP(() => {
+    const titleCleanup = setupTextHover(titleRef.current, "title");
+    const subtitleCleanup = setupTextHover(subtitleRef.current, "subtitle");
+
+    return () => {
+      titleCleanup && titleCleanup();
+      subtitleCleanup && subtitleCleanup();
+    };
+  }, []);
+
+  return (
+    <section id="welcome">
+      <p ref={subtitleRef}>
+        {renderText("Hey, I'm Hossein! Welcome to my", "text-3xl font-georama")}
+      </p>
+      <h1 ref={titleRef} className="mt-7">
+        {" "}
+        {renderText("Portfolio", "text-6xl md:text-9xl font-georama")}
+      </h1>
+
+      <div className="small-screen">
+        <p>Please Check my Portfolio via Desktop/Tablet</p>
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
